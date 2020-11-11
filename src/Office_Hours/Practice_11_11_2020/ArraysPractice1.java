78fc0ced26de6d671ee571c82e536072be4fc369
abc <80joys@gmail.com>
2020-11-11 14:37:57 -0600

    office_hours: practice 11_11_2020-update

--- src/Office_Hours/Practice_11_11_2020/ArraysPractice1.java
+++ src/Office_Hours/Practice_11_11_2020/ArraysPractice1.java
@@ -1,2 +1,47 @@
-package Office_Hours.Practice_11_11_2020;public class ArraysPractice1 {
+package Office_Hours.Practice_11_11_2020;
+
+public class ArraysPractice1 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5,6,7,8,9};
+        int[] arr2 = {10,11,12,13,15,1};
+
+        for (int j=0; j<=arr1.length-1;j++){ // iterates the arr1 indexes
+            int element = arr1[j]; //1
+            for (int i =0;i<=arr2.length-1;i++){ // iterates the arr2 indexes
+                if (element==arr2[i]){
+                    System.out.println(element);
+                    break;
+                }
+
+            }
+        }
+
+        System.out.println("===========================");
+
+        for (int element : arr1){
+
+            for (int each: arr2){
+                if (each==element){
+                    System.out.println(element);
+
+                }
+            }
+        }
+
+
+
+    }
 }
+
+/*
+Write a program that can print out the common elements from two integer array
+		Ex:
+			arr1: {1,2,3,4,5}
+			arr2: {4,5,6,7,8}
+			output:
+				4 5
+	solution 1:
+			DO NOT use any for each loop
+	solution 2:
+			MUST use for each loops
+ */
